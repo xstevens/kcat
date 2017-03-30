59e50f37755e503b3b252e33f977475153c160b2
Xavier Stevens <xavier@simple.com>
2017-03-30 11:25:41 -0700

    Removing build.rs since it's no longer needed

--- build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    // if we're on Mac OS X we'll kindly add DYLD_FALLBACK_LIBRARY_PATH to rustc's
-    // linker search path
-    if let Some(dyld_fallback_path) = option_env!("DYLD_FALLBACK_LIBRARY_PATH") {
-        println!("cargo:rustc-link-search=native={}", dyld_fallback_path)
-    }
-}
